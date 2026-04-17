49c3bccd67730eb81962b2aaf078ee259059b8f2
luiz felipe <98718352+cyberxd12@users.noreply.github.com>
2026-04-17 19:59:32 -0300

    correção v2
    estava mais errado que antes

--- atv25.c
+++ atv25.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 #include <stdbool.h>
+#define tamanho 5
 
-
-int caunt =0;
+int code =0;
 
 struct nme{
     char nome[50];
     int quantidade;
 };
 
-
-struct nme estoque[60];
+struct nme estoque[tamanho];
 
 int opcao;
 
 void cadastro(){
-    printf("\nnome do produto: ");
-    scanf(" %s", &estoque[caunt].nome);
-    printf("quantidade do produto: ");
-    scanf(" %i", &estoque[caunt].quantidade);
-    printf("o id desse produto é %i\n\n", caunt);
+    if(code < tamanho){
+        code++;
+        printf("\nnome do produto: ");
+        scanf(" %s", &estoque[code].nome);
+        printf("quantidade do produto: ");
+        scanf(" %i", &estoque[code].quantidade);
+        printf("o id desse produto é %i\n\n", code);
+    }else{
+        printf("armazenamento cheio!\n");
+    }
 }
 
 void busca(int id){
-    printf("produto %i\nnome: %s\nquantidade: %i\n", caunt, estoque[caunt].nome,estoque[caunt].quantidade);
+    printf("produto %i\nnome: %s\nquantidade: %i\n\n", code, estoque[code].nome,estoque[code].quantidade);
 }
 
 void menu(int opc){
@@ -31,8 +35,7 @@ void menu(int opc){
     opcao = opc;
     switch (opc){
         case 1:
-            caunt++;
-            cadastro(caunt);
+            cadastro(code);
         break;
 
         case 2:
@@ -54,7 +57,7 @@ void menu(int opc){
 
 int main() {
     do{
-        printf("Escolha uma opção:\n1- Cadastrar produto\n2- Buscar produto\n3- Sair do sistema\n4 - \n\n");
+        printf("Escolha uma opção:\n1- Cadastrar produto\n2- Buscar produto\n3- Sair do sistema\n\n");
         scanf("%i", &opcao);
         menu(opcao);
     }while(opcao != 3);
